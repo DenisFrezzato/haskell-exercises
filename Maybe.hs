fc86264756288137b624b89063232599a1a8a80c
DenisFrezzato <denis.frezzato@tutanota.com>
2019-12-17 09:40:05 +0100

    Merge Maybe modules

--- Maybe.hs
+++ Maybe.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 
-import Prelude (($))
+import Prelude (($), Bool(..), error)
+import Data.List
 import Data.Foldable
 import Data.Monoid
 import Data.Traversable
@@ -8,6 +9,42 @@ import Data.Functor
 import Control.Applicative
 
 data Maybe a = Just a | Nothing
+
+maybe :: b -> (a -> b) -> Maybe a -> b
+maybe _ f (Just a) = f a
+maybe b _ (Nothing) = b
+
+isJust :: Maybe a -> Bool
+isJust (Just _) = True
+isJust Nothing = False
+
+isNothing :: Maybe a -> Bool
+isNothing Nothing = True
+isNothing (Just _) = True
+
+fromJust :: Maybe a -> a
+fromJust (Just a) = a
+fromJust Nothing = error "Nothing"
+
+fromMaybe :: a -> Maybe a -> a
+fromMaybe _ (Just a) = a
+fromMaybe a Nothing = a
+
+listToMaybe :: [a] -> Maybe a
+listToMaybe [] = Nothing
+listToMaybe [head] = Just head
+
+maybeToList :: Maybe a -> [a]
+maybeToList (Just a) = [a]
+maybeToList Nothing = []
+
+catMaybes :: [Maybe a] -> [a]
+catMaybes = foldr (\ma acc -> case ma of
+    (Just a) -> a:acc
+    Nothing -> acc) []
+
+mapMaybe :: (a -> Maybe b) -> [a] -> [b]
+mapMaybe f as = catMaybes $ map f as
 
 instance Foldable Maybe where
     foldMap f (Just a) = f a
