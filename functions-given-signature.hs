227ff87fa7af521c9dbed3f82a2b13aeb9b69b71
DenisFrezzato <denis.frezzato@tutanota.com>
2019-12-19 18:54:32 +0100

    Exercise 9 and 10

--- functions-given-signature.hs
+++ functions-given-signature.hs
@@ -4,9 +4,14 @@ identity a = a
 tuple :: a -> (a, a)
 tuple a = (a, a)
 
-map :: (a -> b) -> a -> b
-map f a = f a
+map' :: (a -> b) -> a -> b
+map' f a = f a
 
 tupleMap :: (a -> b -> c) -> (a, b) -> c
 tupleMap f (a, b) = f a b
 
+exercise9 :: Applicative f => [(f a, b)] -> f [(a, b)]
+exercise9 = exercise10
+
+exercise10 :: (Traversable t, Applicative f) => t (f a, b) -> f (t (a, b))
+exercise10 = traverse (\(fa, b) -> fmap (\a -> (a, b)) fa)
