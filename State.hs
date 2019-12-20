01f74f6ddc730f3379d0e5e182875c6c1e0a6e60
DenisFrezzato <denis.frezzato@tutanota.com>
2019-12-20 14:33:45 +0100

    Implement Monad for State

--- State.hs
+++ State.hs
@@ -3,12 +3,19 @@
 import Prelude (($), (.), snd)
 import Data.Functor
 import Control.Applicative
+import Control.Monad
 
 newtype State s a = State (s -> (s, a))
 
 instance Functor (State s) where
-    fmap f (State a) = State (\s -> fmap f $ a s)
+    fmap f (State a) = State $ fmap f . a
 
 instance Applicative (State s) where
     pure x = State (\s -> (s, x))
     State fs <*> State a = State (\s -> fmap (snd $ fs s) $ a s)
+
+instance Monad (State s) where
+    return x = State (\s -> (s, x))
+    State a >>= k = State (\s -> 
+        let (State b) = k $ snd $ a s
+        in b s)
