08e6a2acc227aa5095acbeee9031fb8994c28f56
DenisFrezzato <denis.frezzato@tutanota.com>
2019-12-20 15:46:23 +0100

    Squashed commit of the following:
    commit 3f9265456207f507d4d3055367ba77b4a683a2f9
    Author: DenisFrezzato <denis.frezzato@tutanota.com>
    Date:   Fri Dec 20 15:42:59 2019 +0100
    
        Implement Monad
    
    commit ea358584c67a8387183a7b5c016a891a19c6e8a1
    Author: DenisFrezzato <denis.frezzato@tutanota.com>
    Date:   Thu Dec 19 19:36:00 2019 +0100
    
        Implement Functor and Applicative

---
+++ RWS.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+import Prelude (($))
+import Data.Monoid
+import Data.Functor
+import Control.Applicative
+import Control.Monad
+
+newtype RWS r w s a = RWS { runRWS :: r -> s -> (a, s, w) }
+
+instance Functor (RWS r w s) where
+    fmap f (RWS x) = RWS (\r -> \s -> 
+        let (a, _, w) = x r s
+        in (f a, s, w))
+
+instance Monoid w => Applicative (RWS r w s) where
+    pure x = RWS (\r -> \s -> (x, s, mempty))
+    RWS fab <*> RWS fa = RWS (\r -> \s ->
+        let (b, _, wb) = fab r s
+            (a, _, wa) = fa r s
+        in (b a, s, wb <> wa))
+
+instance Monoid w => Monad (RWS r w s) where
+    return x = RWS (\r -> \s -> (x, s, mempty))
+    RWS x >>= k = RWS (\r -> \s ->
+        let (a, _, wa) = x r s
+            (b, _, wb) = runRWS (k a) r s
+        in (b, s, wa <> wb))
