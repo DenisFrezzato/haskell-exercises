9d115aaa8e50b6e9b594a90e7839b781ee548ca6
DenisFrezzato <denis.frezzato@tutanota.com>
2019-12-21 12:19:24 +0100

    Minor refactor

--- Tree.hs
+++ Tree.hs
@@ -10,20 +10,20 @@ data Tree a = Leaf | Tree a (Tree a) (Tree a)
 
 instance Foldable Tree where
     foldMap _ Leaf = mempty
-    foldMap f (Tree n t' t'') = f n <> foldMap f t' <> foldMap f t''
+    foldMap f (Tree a tl tr) = f a <> foldMap f tl <> foldMap f tr
 
 instance Functor Tree where
     fmap _ Leaf = Leaf
-    fmap f (Tree a t' t'') = Tree (f a) (fmap f t') (fmap f t'')
+    fmap f (Tree a tl tr) = Tree (f a) (fmap f tl) (fmap f tr)
 
 instance Applicative Tree where
     pure x = Tree x Leaf Leaf
-    Tree f ft' ft'' <*> (Tree a t' t'') = Tree (f a) (ft' <*> t') (ft'' <*> t'')
+    Tree f ftl ftr <*> (Tree a tl tn) = Tree (f a) (ftl <*> tl) (ftr <*> tr)
     _ <*> Leaf = Leaf
     Leaf <*> _ = Leaf
 
 instance Traversable Tree where
     traverse _ Leaf = pure Leaf
-    traverse f (Tree a t' t'') = fmap Tree (f a) <*> traverse f t' <*> traverse f t'' 
+    traverse f (Tree a tl tr) = fmap Tree (f a) <*> traverse f tl <*> traverse f tr 
 
 
