cdf1065231a03a7535002890d53663b534d5b3f5
DenisFrezzato <denis.frezzato@tutanota.com>
2019-12-26 17:18:41 +0100

    Refactor using async

--- concurrency/exercises.hs
+++ concurrency/exercises.hs
@@ -1,4 +1,5 @@
 import Control.Concurrent
+import Control.Concurrent.Async
 import Control.Concurrent.MVar
 import Control.Exception
 import System.Timeout
@@ -10,19 +11,13 @@ handleThreadEnd :: Either SomeException a -> IO ()
 handleThreadEnd (Left e) = print e
 handleThreadEnd _ = pure ()
 
-forkIO' :: IO () -> IO (MVar ())
-forkIO' io = do
-    mvar <- newEmptyMVar
-    threadId <- forkFinally io (\e -> handleThreadEnd e >> putMVar mvar ())
-    return mvar
-
 main = do
-    mvar1 <- forkIO' $ do
+    a1 <- async $ do
         m <- timeout 2000000 $ threadDelayMS 2500
         case m of
           Just _ -> putStrLn "Forked IO #1 has done."
           Nothing -> putStrLn "Forked IO #1 has timed out."
-    mvar2 <- forkIO' $ threadDelayMS 1500 >> putStrLn "Forked IO #2 has done."
-    mvar3 <- forkIO' $ threadDelayMS 1000 >> putStrLn "Forked IO #3 has done."
+    a2 <- async $ threadDelayMS 1500 >> putStrLn "Forked IO #2 has done."
+    a3 <- async $ threadDelayMS 1000 >> putStrLn "Forked IO #3 has done."
     putStrLn "Waiting..."
-    sequence_ [takeMVar mvar1, takeMVar mvar2, takeMVar mvar3]
+    sequence_ [wait a1, wait a2, wait a3]
