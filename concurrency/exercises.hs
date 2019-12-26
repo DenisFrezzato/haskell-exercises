26b3b4661711cb9ce77c66cdad3d36ea34f2572c
DenisFrezzato <denis.frezzato@tutanota.com>
2019-12-26 17:12:26 +0100

    Refactor using timeout

--- concurrency/exercises.hs
+++ concurrency/exercises.hs
@@ -1,6 +1,7 @@
 import Control.Concurrent
 import Control.Concurrent.MVar
 import Control.Exception
+import System.Timeout
 
 threadDelayMS :: Int -> IO ()
 threadDelayMS ms = threadDelay $ ms * 1000
@@ -9,15 +10,19 @@ handleThreadEnd :: Either SomeException a -> IO ()
 handleThreadEnd (Left e) = print e
 handleThreadEnd _ = pure ()
 
-forkIO' :: IO () -> IO (ThreadId, MVar ())
+forkIO' :: IO () -> IO (MVar ())
 forkIO' io = do
     mvar <- newEmptyMVar
     threadId <- forkFinally io (\e -> handleThreadEnd e >> putMVar mvar ())
-    return (threadId, mvar)
+    return mvar
 
 main = do
-    (threadId1, mvar1) <- forkIO' $ threadDelayMS 2500 >> putStrLn "Forked IO #1 has done."
-    (_, mvar2) <- forkIO' $ threadDelayMS 1500 >> putStrLn "Forked IO #2 has done."
-    (_, mvar3) <- forkIO' $ threadDelayMS 1000 >> putStrLn "Forked IO #3 has done."
-    throwTo threadId1 $ ErrorCall "Forked IO #1 has been halted."
+    mvar1 <- forkIO' $ do
+        m <- timeout 2000000 $ threadDelayMS 2500
+        case m of
+          Just _ -> putStrLn "Forked IO #1 has done."
+          Nothing -> putStrLn "Forked IO #1 has timed out."
+    mvar2 <- forkIO' $ threadDelayMS 1500 >> putStrLn "Forked IO #2 has done."
+    mvar3 <- forkIO' $ threadDelayMS 1000 >> putStrLn "Forked IO #3 has done."
+    putStrLn "Waiting..."
     sequence_ [takeMVar mvar1, takeMVar mvar2, takeMVar mvar3]
